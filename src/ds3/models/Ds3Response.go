1a7f427cbca252b0b2866b86383f3d8af2770683
Hans-Peter Klett <hansk@spectralogic.com>
2014-04-07 10:23:29 -0600

    Added error response parsing to the ds3 response handling.

--- src/ds3/models/Ds3Response.go
+++ src/ds3/models/Ds3Response.go
@@ -7,31 +7,30 @@ import (
     "encoding/xml"
 )
 
-//TODO: read the response contents to make better error messages.
 func checkStatusCode(response *http.Response, expectedStatusCode int) error {
     if response.StatusCode == expectedStatusCode {
         return nil
     } else {
-        // Return a status code error
-        return fmt.Errorf(
-            "Expected HTTP status code %d but got %d.",
-            expectedStatusCode,
-            response.StatusCode,
-        )
+        return buildBadStatusCodeError(response, expectedStatusCode)
     }
 }
 
 func readResponseBody(response *http.Response, expectedStatusCode int, body interface{}) error {
-    stream := response.Body
-    defer stream.Close()
+    if response.Body != nil {
+        defer response.Body.Close()
+    }
 
     // Check the status code.
     if err := checkStatusCode(response, expectedStatusCode); err != nil {
         return err
     }
 
+    return parseResponseBody(response, body)
+}
+
+func parseResponseBody(response *http.Response, body interface{}) error {
     // Get the bytes or forward the error.
-    bytes, err := ioutil.ReadAll(stream)
+    bytes, err := ioutil.ReadAll(response.Body)
     if err != nil {
         return err
     }
@@ -41,7 +40,54 @@ func readResponseBody(response *http.Response, expectedStatusCode int, body inte
         return err
     }
 
-    // Return the result.
     return nil
+}
+
+func buildBadStatusCodeError(response *http.Response, expectedStatusCode int) *BadStatusCodeError {
+    var errorBody Error
+    var errorBodyPtr *Error
+
+    // Parse the body and if it worked then use the structure.
+    err := parseResponseBody(response, &errorBody)
+    if err == nil {
+        errorBodyPtr = &errorBody
+    }
+
+    // Return the bad status code entity.
+    return &BadStatusCodeError{
+        expectedStatusCode,
+        response.StatusCode,
+        errorBodyPtr,
+    }
+}
+
+type BadStatusCodeError struct {
+    ExpectedStatusCode int
+    ActualStatusCode int
+    ErrorBody *Error
+}
+
+type Error struct {
+    Code string
+    Message string
+    Resource string
+    RequestId string
+}
+
+func (err BadStatusCodeError) Error() string {
+    if err.ErrorBody != nil {
+        return fmt.Sprintf(
+            "Received a status code of %d when %d was expected. Error message: \"%s\"",
+            err.ActualStatusCode,
+            err.ExpectedStatusCode,
+            err.ErrorBody.Message,
+        )
+    } else {
+        return fmt.Sprintf(
+        "Received a status code of %d when %d was expected. Could not parse the response for additional information.",
+            err.ActualStatusCode,
+            err.ExpectedStatusCode,
+        )
+    }
 }
 
