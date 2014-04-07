b61bb7d6eb1f47867a145ce9627429977fe8faeb
Hans-Peter Klett <hansk@spectralogic.com>
2014-04-07 10:25:51 -0600

    Consolidated a few files.

--- src/ds3/ds3.go
+++ src/ds3/ds3.go
@@ -1,8 +1,38 @@
 package ds3
 
-import "ds3/net"
+import (
+    "ds3/net"
+    "net/url"
+)
 
 type Client struct {
     netLayer net.Network
+}
+
+type Builder struct {
+    connectionInfo *net.ConnectionInfo
+}
+
+func NewBuilder(endpoint *url.URL, creds net.Credentials) *Builder {
+    return &Builder{&net.ConnectionInfo{
+        Endpoint: *endpoint,
+        Creds: creds,
+        RedirectRetryCount: 5,
+        Proxy: nil,
+    }}
+}
+
+func (builder *Builder) WithProxy(proxy *url.URL) *Builder {
+    builder.connectionInfo.Proxy = proxy
+    return builder
+}
+
+func (builder *Builder) WithRedirectRetryCount(count int) *Builder {
+    builder.connectionInfo.RedirectRetryCount = count
+    return builder
+}
+
+func (builder *Builder) Build() *Client {
+    return &Client{net.NewHttpNetwork(builder.connectionInfo)}
 }
 
