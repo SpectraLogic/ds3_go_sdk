0a3af4bd8550053ae2ef21a32146f7267d4f2927
Rachel Tucker <rachelt@spectralogic.com>
2017-07-28 13:22:32 -0600

    Created 3 more tests including bulk put

--- src/ds3_integration/utils/testUtils.go
+++ src/ds3_integration/utils/testUtils.go
@@ -9,10 +9,123 @@ import (
     "errors"
     "ds3/buildclient"
     "log"
+    "io"
+    "ds3_utils/ds3Testing"
+    "bytes"
 )
 
 var bookPath = "./resources/books/"
 var nilResponse error = errors.New("Received an unexpected nil response.")
+var BookTitles = []string{ "beowulf.txt", "sherlock_holmes.txt", "tale_of_two_cities.txt", "ulysses.txt"}
+
+func VerifyBookContent(t *testing.T, bookName string, content io.ReadCloser) {
+    actual, loadErr := LoadBook(bookName)
+    ds3Testing.AssertNilError(t, loadErr)
+
+    bs, readErr := ioutil.ReadAll(content)
+    ds3Testing.AssertNilError(t, readErr)
+    if bytes.Compare(bs, actual) != 0 {
+        t.Error("Retrieved book does not match uploaded book.")
+    }
+}
+
+func ConvertObjectsIntoDs3Objects(objects []models.Contents) []models.Ds3Object {
+    var ds3Objects []models.Ds3Object
+    for _, obj := range objects{
+        curDs3Object := models.Ds3Object{ Name:*obj.Key, Size:obj.Size, }
+        ds3Objects = append(ds3Objects, curDs3Object)
+    }
+    return ds3Objects
+}
+
+//Puts the test books onto the BP
+func PutTestBooks(client *ds3.Client, bucketName string) error {
+    books, bookErr := GetResourceBooks()
+    if bookErr != nil {
+        return bookErr
+    }
+
+    ds3Objects, objErr := ConvertBooksToDs3Objects(books)
+    if objErr != nil {
+        return objErr
+    }
+
+    // Create bulk put job
+    bulkPut, bulkPutErr := client.PutBulkJobSpectraS3(models.NewPutBulkJobSpectraS3Request(bucketName, ds3Objects))
+    if bulkPutErr != nil {
+        return bulkPutErr
+    }
+
+    for _, chunk := range bulkPut.MasterObjectList.Objects {
+        allocateChunk, allocateErr := client.AllocateJobChunkSpectraS3(models.NewAllocateJobChunkSpectraS3Request(chunk.ChunkId))
+        if allocateErr != nil {
+            return allocateErr
+        }
+        for _, obj := range allocateChunk.Objects.Objects {
+            content := books[*obj.Name]
+            _, putObjErr := client.PutObject(models.NewPutObjectRequest(bucketName, *obj.Name, content).
+                WithOffset(obj.Offset).
+                WithJob(bulkPut.MasterObjectList.JobId))
+
+            if putObjErr != nil {
+                return putObjErr
+            }
+        }
+    }
+    return nil
+}
+
+func DeleteBucketContents(client *ds3.Client, bucketName string) {
+    //Get the contents of bucket
+    getBucket, getBucketErr := client.GetBucket(models.NewGetBucketRequest(bucketName))
+    if getBucketErr != nil {
+        log.Printf("WARNING: Cleanup error when attempting to get contents of bucket '%s': '%s'.", bucketName, getBucketErr.Error())
+        return
+    }
+    for _, obj := range getBucket.ListBucketResult.Objects {
+        deleteErr := DeleteObject(client, bucketName, *obj.Key)
+        if deleteErr != nil {
+            log.Printf("WARNING: Cleanup error when attempting to delete object '%s' from bucket '%s': '%s'.", *obj.Key, bucketName, deleteErr.Error())
+        }
+    }
+}
+
+func GetResourceBooks() (map[string]networking.ReaderWithSizeDecorator, error) {
+    result := make(map[string]networking.ReaderWithSizeDecorator)
+    for _, title := range BookTitles {
+        curStream, err := GetResourceBookAsStream(title)
+        if err != nil {
+            return nil, err
+        }
+        result[title] = *curStream
+    }
+    return result, nil
+}
+
+func ConvertBooksToDs3Objects(books map[string]networking.ReaderWithSizeDecorator) ([]models.Ds3Object, error) {
+    var ds3Objects []models.Ds3Object
+    for title, stream := range books {
+        size, err := stream.Size()
+        if err != nil {
+            return nil, err
+        }
+        var curObj models.Ds3Object = models.Ds3Object{
+            Name:title,
+            Size:size,
+        }
+        ds3Objects = append(ds3Objects, curObj)
+    }
+    return ds3Objects, nil
+}
+
+func GetResourceBookAsStream(book string) (*networking.ReaderWithSizeDecorator, error) {
+    content, err := LoadBook(book)
+    if err != nil {
+        return nil, err
+    }
+    stream := networking.BuildByteReaderWithSizeDecorator(content)
+    return &stream, nil
+}
 
 // Loads the specified book. If an error occurs, it is logged, and the calling
 // test is marked as failed.
@@ -182,9 +295,12 @@ func SetupTestEnv(test_bucket string) (*ds3.Client, error) {
 
 // Tears down the test environment by deleting all contents in the
 // test bucket
-func TeardownTestEnv(client *ds3.Client, test_bucket string) {
+func TeardownTestEnv(client *ds3.Client, testBucket string) {
+    //Delete contents in test bucket
+    DeleteBucketContents(client, testBucket)
+
     //Delete the test bucket
-    err := DeleteBucket(client, test_bucket)
+    err := DeleteBucket(client, testBucket)
     if err != nil {
         log.Print(err)
     }
