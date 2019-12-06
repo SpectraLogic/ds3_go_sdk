b11e23f1e19829513541db90dc9828aec76a33b2
RachelTucker <RachelTucker@users.noreply.github.com>
2019-12-06 11:13:11 -0700

    GOSDK-30: Add a way to skip cert verification (#98)

--- ds3/networking/net.go
+++ ds3/networking/net.go
@@ -1,15 +1,17 @@
 package networking
 
 import (
+    "crypto/tls"
     "net/http"
     "net/url"
     "github.com/SpectraLogic/ds3_go_sdk/ds3/models"
 )
 
 type ConnectionInfo struct {
-    Endpoint    *url.URL
-    Credentials *Credentials
-    Proxy       *url.URL
+    Endpoint                *url.URL
+    Credentials             *Credentials
+    Proxy                   *url.URL
+    IgnoreServerCertificate bool
 }
 
 type Credentials struct {
@@ -27,9 +29,15 @@ type SendNetwork struct {
 }
 
 func NewSendNetwork(connectionInfo *ConnectionInfo) Network {
-    return &SendNetwork{
-        transport: &http.Transport{Proxy: http.ProxyURL(connectionInfo.Proxy)},
+    sendNetwork := &SendNetwork{
+        transport: &http.Transport{
+            Proxy: http.ProxyURL(connectionInfo.Proxy),
+        },
     }
+    if connectionInfo.IgnoreServerCertificate {
+        sendNetwork.transport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+    }
+    return sendNetwork
 }
 
 func (sendNetwork *SendNetwork) Invoke(httpRequest *http.Request) (models.WebResponse, error) {
